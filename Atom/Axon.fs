4595120c2c66a7da552b947c6b24197f7aef9860
Javehdez <javehdez@gmail.com>
2015-01-09 10:36:19 -0600

    Axon forwarding specific observable messages

--- Atom/Axon.fs
+++ Atom/Axon.fs
@@ -2,9 +2,19 @@
 
 open System
 open System.Collections.Generic
+open Nessos.FsPickler
+open Nessos.FsPickler.Json
+open Newtonsoft.Json
+
+type Message<'T> = 
+    {
+      channel : 'T
+      typ : System.Type
+      payload : byte array
+    }
 
 module private __ =
-
+   
     type __<'MsgName,'MsgType when 'MsgName : equality> () =
         static let __ = Dictionary<'MsgName, Event<'MsgType>> ()
         static member Dict () = __
@@ -18,6 +28,25 @@ module private __ =
             let observable = Event<'MsgType> ()
             axons.[axon] <- observable
             observable
+    
+    let Compare (path:obj)  =
+        let serverPattern = [":OnNext";":OnCompleted";":OnError"]
+        let clientPattern = ":OnSubscribed"
+        match path with
+        | :? string as strPath ->
+            let serverForward = List.fold (fun state pattern -> state || strPath.Contains(pattern)) false serverPattern
+            let clientForward = strPath.Contains(clientPattern)
+            (serverForward,"serverSend"),(clientForward,"clientSend")
+        | _ -> (false,""),(false,"")
+
+    let CreateMessage<'MsgName,'MsgType when 'MsgName : equality> (path:'MsgName) (msg:'MsgType) =
+        let json = FsPickler.CreateJson (omitHeader = true)
+        let typ = typeof<'MsgType>
+        let ms = new System.IO.MemoryStream ()
+        json.Serialize<'MsgType>(ms,msg)
+        let payload = ms.ToArray ()
+        let message = {channel = path;typ = typ;payload = payload}
+        message
 
 open __
 
@@ -26,3 +55,10 @@ let observe<'MsgName,'MsgType when 'MsgName : equality> path =
 
 let trigger<'MsgName,'MsgType when 'MsgName : equality> path msg =
     (get<'MsgName,'MsgType> path).Trigger msg
+    match Compare path with
+    | (true,channel),(_)
+    | (_),(true,channel) -> 
+        let message = CreateMessage<'MsgName,'MsgType> path msg
+        (get<string,Message<'MsgName>> channel).Trigger message
+    | _ -> ()
+
